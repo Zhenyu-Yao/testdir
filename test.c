87d80481173a47b7c6070e42fed82e20bad02a73
100ask <user@100ask.com>
2021-01-10 17:51:25 +0800

    加了一些注释

--- test.c
+++ test.c
@@ -1,12 +1,20 @@
 #include<stdio.h>
 
+
+
 int main(int argc,char **argv)
 {
-	printf("12345\n");
+
+	printf("12345_new_try\n");
 	typedef void (*ptr)();
 	(*(ptr)0)();
 	
 	return 0;
+
 }
+
+
+//加点新东西
+
 
 
