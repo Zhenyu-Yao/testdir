910d1c48f3d27be17265051d7ea12e79487ff9cd
100ask <user@100ask.com>
2021-01-11 23:30:40 +0800

    For GDB coredump demo.

--- test.c
+++ test.c
@@ -1,8 +1,22 @@
 #include<stdio.h>
 
+
+void sub(int x, char * y)
+{
+
+	printf("Start  of sub.\n");
+	printf("argc=%d\n",x);
+	printf("argv[1]=%s\n",y);
+	printf("End  of sub.\n");
+
+}
+
+
 int main(int argc,char **argv)
 {
 	printf("12345\n");
+	sub(argc,argv[1]);
+	printf("Return of sub.\n");
 	typedef void (*ptr)();
 	(*(ptr)0)();
 	
